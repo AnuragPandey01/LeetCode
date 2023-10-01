90549c9d31a1337bef804c660477f9e4d9470878
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-01 22:04:39 +0530

    Time: 1 ms (99.38%), Space: 44 MB (9.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,31 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+    // Create a HashMap to store the values and their corresponding indices.
+    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    // Initialize an array to store the result, initially with zeros.
+    int[] ans = new int[2];
+    
+    // Loop through the input array 'nums'.
+    for (int i = 0; i < nums.length; i++) {
+        int curr = nums[i]; // Current number from the array.
         
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int[] ans = new int[2];
-        for(int i=0;i<nums.length;i++){
-            int curr = nums[i];
-            if(map.containsKey(target-curr)){
-                ans[0] = map.get(target-curr);
-                ans[1] = i;
-                break;
-            }
-            map.put(curr,i);
+        // Check if there is a complement value (target - curr) in the HashMap.
+        if (map.containsKey(target - curr)) {
+            // If a complement is found, set the indices in the result array.
+            ans[0] = map.get(target - curr); // Index of the complement.
+            ans[1] = i; // Current index.
+            
+            // Break out of the loop since we've found the answer.
+            break;
         }
-
-        return ans;
+        
+        // If the complement is not found, store the current number and its index in the HashMap.
+        map.put(curr, i);
     }
+
+    // Return the result array containing the indices of the two numbers that add up to the target.
+    return ans;
+}
+
 }
