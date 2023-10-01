5c088f5ce9e49a9c8df7a366817109ac1b830062
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-01 23:41:51 +0530

    Time: 1 ms (100%), Space: 57.6 MB (84.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,23 +1,27 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE; // Initialize the maximum sum to the smallest possible integer value.
+        int sum = 0; // Initialize a variable to keep track of the current sum.
 
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
+        // Iterate through the array.
+        for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i]; // Current element being considered.
 
-        for(int i=0;i<nums.length;i++){
-            int curr =nums[i];
+            sum += curr; // Add the current element to the current sum.
 
-            
-            sum += curr;
-
-            if(sum>max){
+            if (sum > max) {
+                // If the current sum is greater than the current maximum, update the maximum.
                 max = sum;
             }
-            
-            if(sum<0){
+
+            if (sum < 0) {
+                // If the current sum becomes negative, reset it to zero.
                 sum = 0;
             }
         }
+
+        // 'max' now contains the maximum subarray sum after processing the entire array.
         return max;
     }
+
 }
