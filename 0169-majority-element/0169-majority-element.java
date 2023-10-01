cf0f073184f9a1ebbe2711ea8581aac34b2ad915
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-01 23:13:22 +0530

    Time: 1 ms (99.85%), Space: 48.3 MB (60.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,28 @@
 class Solution {
     public int majorityElement(int[] nums) {
-
+        // Initialize variables to keep track of the current majority element and its count.
         int count = 1;
-        int ele = nums[0];
+        int ele = nums[0]; // Assume the first element as the initial candidate for majority.
 
-        for(int i=1;i<nums.length;i++){
-            int curr = nums[i];
+        // Iterate through the array starting from the second element.
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i]; // Current element being considered.
 
-            if(count == 0){
+            // If the count of the current majority element reaches zero, update the candidate.
+            if (count == 0) {
                 ele = nums[i];
                 count++;
-            }else if(curr == ele){
+            } else if (curr == ele) {
+                // If the current element is the same as the current majority candidate, increment the count.
                 count++;
-            }else{
+            } else {
+                // If the current element is different, decrement the count.
                 count--;
             }
         }
 
+        // The 'ele' variable now contains the majority element after processing the entire array.
         return ele;
     }
+
 }
