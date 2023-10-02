132e6cb86caceeade553fdab33289e1f16fc224b
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-02 09:55:31 +0530

    Time: 1 ms (25.01%), Space: 41.9 MB (73.64%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,31 +1,39 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-
         int n = nums.length;
         int index = -1;
-        for(int i =n-2; i>=0; i-- ){
-            if(nums[i]<nums[i+1]){
+
+        // Find the first index 'index' from right to 
+        // left where nums[index] < nums[index+1].
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 index = i;
                 break;
             }
         }
 
-        if(index == -1){
+        // If 'index' remains -1, it means the entire array 
+        // is in descending order, so reverse it to get the smallest permutation.
+        if (index == -1) {
             Arrays.sort(nums);
             return;
         }
 
-        for(int i= n-1;i>= 0;i--){
-            if(nums[i] > nums[index]){
-                swap(nums,i,index);
+        // Find the first element from the right that is greater than 
+        // nums[index] and swap them.
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums, i, index);
                 break;
             }
         }
 
-        Arrays.sort(nums,index+1,n);
+        // sort right to ensure its the clossest perm
+        Arrays.sort(nums, index + 1, n);
     }
 
-    public static void swap(int[] nums, int a ,int b){
+    // Helper function to swap two elements in the 'nums' array.
+    public static void swap(int[] nums, int a, int b) {
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
