7465941b7bfc20d271a693f855ba11a75bb416da
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-02 07:35:38 +0530

    Time: 78 ms (13.82%), Space: 44.3 MB (13.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,31 +1,37 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // Create a HashMap to keep track of characters and their presence in the current substring.
+        HashMap<Character, Boolean> map = new HashMap<>();
 
-        HashMap<Character,Boolean> map = new HashMap<>();
+        int n = s.length(); // Get the length of the input string.
+        int maxLen = 0; // Initialize the maximum substring length.
+        int count = 0; // Initialize a variable to count the length of the current substring.
+        int start = 0; // Initialize the start index of the current substring.
+        int i = 0; // Initialize the current index for iterating through the string.
 
-        int n = s.length();
-        int maxLen = 0;
-        int count = 0;
-        int start = 0;
-        int i =0;
-        while(i<n){
-
-            if(map.containsKey(s.charAt(i))){
+        // Iterate through the string.
+        while (i < n) {
+            if (map.containsKey(s.charAt(i))) {
+                // If the current character is already in the substring, reset the substring.
                 count = 0;
-                i = start+1;
-                start = i;
-                map.clear();
+                i = start + 1; // Move to the character after the first occurrence of the repeated character.
+                start = i; // Update the start index for the new substring.
+                map.clear(); // Clear the HashMap to track the new substring.
             }
 
-            count++;
-            if(count>maxLen){
+            count++; // Increment the length of the current substring.
+
+            if (count > maxLen) {
+                // If the current substring is longer than the previously recorded maximum, update it.
                 maxLen = count;
             }
-            map.put(s.charAt(i),true);
-            i++;
+
+            map.put(s.charAt(i), true); // Mark the current character as present in the substring.
+            i++; // Move to the next character.
         }
 
+        // 'maxLen' now contains the length of the longest substring without repeating characters.
         return maxLen;
-
     }
+
 }
