e1c2334327d3d566e63ade197c2e0d88ffadbcbc
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-16 21:29:12 +0530

    Time: 0 ms (100%), Space: 48.3 MB (98.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,42 +1,48 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
-        int low =0;
-        int high = nums.length-1;
-        int n = nums.length;
-        int ans = -1;
+    int low = 0;               
+    int high = nums.length - 1; 
+    int n = nums.length;       
+    int ans = -1;           
 
-        while(low <= high){
-
-            if(low  == high){
-                ans =  nums[low];
-                break;
-            }
-
-            int mid = low +(high-low)/2;
-
-            if( mid >0  && nums[mid] ==  nums[mid-1]){
-                int leftIndex = mid-2;
-                if(leftIndex >= 0 && leftIndex%2==0){
-                    //since leftside has odd no. elements we consider it
-                    high = leftIndex;
-                }else{
-                    low = mid+1;
-                }
-            }else if(mid < n-1 && nums[mid] == nums[mid+1]){
-                int rightIndex = mid+2;
-                if(rightIndex < nums.length && (mid-1)%2 != 0){
-                    low = rightIndex;
-                }else{
-                    high =  mid-1;
-                }
-            }else{
-                ans = nums[mid];
-                break;
-            }
-
+    while (low <= high) {
+        if (low == high) {
+            // If low and high pointers meet, we've found the single element.
+            ans = nums[low];
+            break;
         }
 
-        return ans;
+        int mid = low + (high - low) / 2; // Calculate the middle index.
+
+        if (mid > 0 && nums[mid] == nums[mid - 1]) {
+            // If the middle element is equal to the element before it, check the left side.
+            int leftIndex = mid - 2;
+            if (leftIndex >= 0 && leftIndex % 2 == 0) {
+                // If the left side has an odd number of elements, the single element is on the left.
+                high = leftIndex;
+            } else {
+                // Otherwise, search the right side.
+                low = mid + 1;
+            }
+        } else if (mid < n - 1 && nums[mid] == nums[mid + 1]) {
+            // If the middle element is equal to the element after it, check the right side.
+            int rightIndex = mid + 2;
+            if (rightIndex < n && (mid - 1) % 2 != 0) {
+                // If the right side has an odd number of elements, the single element is on the right.
+                low = rightIndex;
+            } else {
+                // Otherwise, search the left side.
+                high = mid - 1;
+            }
+        } else {
+            // If the middle element is not equal to the elements before or after it, it's the single element.
+            ans = nums[mid];
+            break;
+        }
     }
+
+    return ans; // Return the single element.
+}
+
 }
