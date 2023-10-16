62a1793146b584f831ed0f854ce84b8d2ed554fa
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-16 22:01:21 +0530

    Time: 0 ms (100%), Space: 40.8 MB (94.33%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,27 +2,37 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
 
-        if(n == 1) return 0;
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n-1] > nums[n-2]) return n-1;
+        if (n == 1) return 0; // If there's only one element, it's the peak.
 
-        int low = 1; int high = n-2;
-        while(low<= high){
-            
-            int mid = low +(high-low)/2;
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+        if (nums[0] > nums[1]) return 0; // Check if the first element is a peak.
+        if (nums[n - 1] > nums[n - 2]) return n - 1; // Check if the last element is a peak.
+
+        int low = 1;  // Initialize the low pointer to the second element.
+        int high = n - 2; // Initialize the high pointer to the second-to-last element.
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // Calculate the middle index.
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                // If the current element is greater than both its neighbors, it's a peak.
                 return mid;
             }
-            if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]){
-                low = mid+1;
-            }
-            else if(nums[mid] > nums[mid+1] && nums[mid] < nums[mid-1]){
-                high = mid-1;
-            }else{
-                low = mid+1;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                // If the current element is greater than the left neighbor but smaller than the right neighbor,
+                // move the low pointer to the right to search for a peak on the right side.
+                low = mid + 1;
+            } else if (nums[mid] > nums[mid + 1] && nums[mid] < nums[mid - 1]) {
+                // If the current element is greater than the right neighbor but smaller than the left neighbor,
+                // move the high pointer to the left to search for a peak on the left side.
+                high = mid - 1;
+            } else {
+                // If neither of the above conditions is met, move the low pointer to the right.
+                low = mid + 1;
             }
         }
 
-        return -1;
+        return -1; // If no peak is found, return -1.
     }
+
 }
