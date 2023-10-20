8ec42cd4f74092d9a642d6aafe094cb32161447d
Anurag Pandey <98679799+AnuragPandey01@users.noreply.github.com>
2023-10-20 09:02:39 +0530

    Time: 22 ms (39.67%), Space: 54.9 MB (5.07%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> ans = new HashSet<>();
+        Set<String> set = new HashSet<>();
+
+        int low =0; int high =0;
+        StringBuilder currStr =  new StringBuilder();
+        while(high<s.length()){
+
+            currStr.append(s.charAt(high));
+            while(high-low+1 > 10){
+                currStr.deleteCharAt(0);
+                low++;
+            }
+            if(high-low+1 == 10 && set.contains(currStr.toString())){
+                ans.add(currStr.toString());
+            }
+            set.add(currStr.toString());
+            high++;
+        }
+        return ans.stream().toList();
+    }
+}
